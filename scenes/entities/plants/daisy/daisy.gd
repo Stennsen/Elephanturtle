658ec813e08a370248d8f6d34fafee93e8544273
Arda Kacar <arda.kacar21@gmail.com>
2023-11-04 17:27:20 +0100

    made daisy prefabs and added cropsound

--- scenes/entities/plants/daisy/daisy.gd
+++ scenes/entities/plants/daisy/daisy.gd
@@ -1,11 +1,15 @@
-extends StaticBody2D
+extends Area2D
 
 
+@onready var crop_sound = $crop_sound
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _input(event):
+	if event.is_action_pressed("crop"):  # Check if the "E" key is pressed
+		crop_sound.play()
+		print("cropped")
