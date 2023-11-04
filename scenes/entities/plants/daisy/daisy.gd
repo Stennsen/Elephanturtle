ce626c973d31478c2a3dd60d28e741885236b0a3
Stennsen <dennis@stennsen.dev>
2023-11-05 00:09:03 +0100

    add wip daisy spawner

--- scenes/entities/plants/daisy/daisy.gd
+++ scenes/entities/plants/daisy/daisy.gd
@@ -10,8 +10,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func destroy_area():
 	queue_free()
+
 func _input(event):
 	if event.is_action_pressed("crop"):  # Check if the "E" key is pressed
 		crop_sound.play()
 		destroy_area()
 		print("cropped")
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
--- scenes/level/diskworld.gd
+++ scenes/level/diskworld.gd
@@ -5,6 +5,8 @@ const ZONE_SPACING = 500	# Spacing of the zones on the map, to calculate the wei
 const MAX_INBALANCE = 0.45	# the maximum +/- value for balance until gameover
 const INBALANCE_MOVEMENT_DIVIDER = 100	# dictates the tipping speed of the platform 
 
+@export var daisy_scene: PackedScene
+
 # tracks the balance of the world
 var balance = -0.45;
 
@@ -20,7 +22,9 @@ func rotate_camera():
 	var camera = get_node("Camera2D")
 	var cur_rotation = camera.global_rotation
 	var rotation_diff = balance - cur_rotation
-	camera.rotate(rotation_diff/INBALANCE_MOVEMENT_DIVIDER)
+	var incoming_rotation = rotation_diff/INBALANCE_MOVEMENT_DIVIDER
+	camera.rotate(incoming_rotation)
+	get_node("Background").rotate(incoming_rotation)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -33,3 +37,21 @@ func _process(delta):
 	#calculate_balance()
 	rotate_camera()
 	pass
+
+# func _on_score_timer_timeout():
+# 	score += 1
+
+func _on_start_timer_timeout():
+	$Flowertimer.start()
+
+func _on_flower_timer_timeout():
+	var daisy = daisy_scene.instanciate()
+	var daisy_spawn_location = get_node("FlowerSpawn/FlowerSpawnLocation")
+	var direction = daisy_spawn_location.rotation + PI/2
+	daisy_spawn_location.progress_ratio = randf()
+	daisy.position = daisy_spawn_location.position
+	daisy.rotation = direction
+	add_child(daisy)
+
+func _on_flowertimer_timeout():
+	pass # Replace with function body.
