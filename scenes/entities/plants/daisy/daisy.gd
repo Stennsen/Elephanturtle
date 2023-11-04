8260d5b937c8c8ae8020ae0562d079570d5e0d66
Arda Kacar <arda.kacar21@gmail.com>
2023-11-04 17:35:20 +0100

    sound file

--- scenes/entities/plants/daisy/daisy.gd
+++ scenes/entities/plants/daisy/daisy.gd
@@ -8,8 +8,10 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-
+func destroy_area():
+	queue_free()
 func _input(event):
 	if event.is_action_pressed("crop"):  # Check if the "E" key is pressed
 		crop_sound.play()
+		destroy_area()
 		print("cropped")
